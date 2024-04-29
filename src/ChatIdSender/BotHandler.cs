342564c43cad20d2272de510b20f9f9df7263174
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-04-29 18:02:43 +0500

    add files

---
+++ src/ChatIdSender/BotHandler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace ChatIdSender
+{
+    public class BotHandler
+    {
+        private readonly string botToken;
+
+        public BotHandler(string token)
+        {
+            botToken = token;
+        }
+
+        public async Task BotHandle()
+        {
+            var botClient = new TelegramBotClient(botToken);
+
+            using CancellationTokenSource cts = new();
+
+            ReceiverOptions receiverOptions = new()
+            {
+                AllowedUpdates = Array.Empty<UpdateType>()
+            };
+
+            botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+
+            var me = await botClient.GetMeAsync();
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+
+            cts.Cancel();
+        }
+
+        private async Task HandlePollingErrorAsync(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task HandleUpdateAsync(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            if (update.Message is not { } message)
+                return;
+            try
+            {
+                await client.SendTextMessageAsync(
+                        chatId: "",
+                        text: message.Chat.Id.ToString(),
+                        cancellationToken: token);
+            } catch
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
--- src/ChatIdSender/obj/Debug/net8.0/ChatIdSender.AssemblyInfo.cs
+++ src/ChatIdSender/obj/Debug/net8.0/ChatIdSender.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatIdSender")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a13537868d29a91992f3cc4823da59c236776e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6abb74be95d8af99ef7b3a59ee63c7826d5d610c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatIdSender")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatIdSender")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
